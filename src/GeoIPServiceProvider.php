3ff6273892542d2080fec0ea541319a041f10d9f
Jeremy Schaffer <jeremys@codenexus.org>
2016-08-21 05:14:40 +0000

    Applied fixes from StyleCI

--- src/GeoIPServiceProvider.php
+++ src/GeoIPServiceProvider.php
@@ -7,25 +7,22 @@ use Codenexus\GeoIP\Console\Commands\UpdateCommand;
 
 class GeoIPServiceProvider extends ServiceProvider
 {
-	public function boot()
-	{
+    public function boot()
+    {
+    }
 
-	}
+    public function register()
+    {
+        $this->app->singleton('geoip', function () {
+            return new GeoIP;
+        });
 
-	public function register()
-	{
-		$this->app->singleton('geoip', function()
-		{
-			return new GeoIP;
-		});
+        $this->app->singleton('command.geoip.update', function () {
+            return new UpdateCommand;
+        });
 
-		$this->app->singleton('command.geoip.update', function()
-		{
-			return new UpdateCommand;
-		});
-
-		$this->commands(
-			'command.geoip.update'
-		);
-	}
+        $this->commands(
+            'command.geoip.update'
+        );
+    }
 }
